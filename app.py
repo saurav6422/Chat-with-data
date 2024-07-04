e2f4d63959d6db6a4d2d25b6fe43fdd5bcd11c11
Saurav Kumar Bakuli <145650305+saurav6422@users.noreply.github.com>
2024-07-04 13:33:28 +0530

    lamda

--- app.py
+++ app.py
@@ -692,12 +692,12 @@ if query:
                     df[column] = df[column].apply(eval(custom_func))
                     st.markdown(f"<p style='font-size: 20px; text-align: center;'>Function applied to <strong>{column}</strong>successfully.</p>", unsafe_allow_html=True)
                     st.dataframe(df.head())
+                    export_format = st.selectbox("Export format", ['CSV', 'Excel', 'JSON'])
+                    if st.button('Export Data'):
+                        export_data(df, export_format)
                 except Exception as e:
                     #st.write(f"Error applying function: {e}")
                     st.markdown(f"<p style='font-size: 20px; text-align: center;'>Error applying function.</p>", unsafe_allow_html=True)
-            export_format = st.selectbox("Export format", ['CSV', 'Excel', 'JSON'])
-            if st.button('Export Data'):
-                export_data(df, export_format)
         else:
             if action not in ["show data", "describe", "filter", "plot", "count rows", 
                     "maximum", "minimum", "average", "sum", "count value", "missing values", 
